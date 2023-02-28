a32f054b1c2a29ccfd20f8e237fe280a83ab2759
escaperadius <chris@escaperadius.com>
2023-02-28 13:21:18 -0500

    poco

--- md5.cpp
+++ md5.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
-#include <string>
+#include "Poco/MD5Engine.h"
+#include "Poco/DigestStream.h"
 
-int main(){
-    std::cout<< "MD5 Hello Qapla "\n";
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{
+    Poco::MD5Engine md5;
+    Poco::DigestOutputStream ds(md5);
+    ds << "abcdefghijklmnopqrstuvwxyz";
+    ds.close();
+    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
+    return 0;
 }
