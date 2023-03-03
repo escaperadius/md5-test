9aaa4c9f444912131a8254b18ba29ee963acc034
escaperadius <chris@escaperadius.com>
2023-03-03 09:42:37 -0500

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -8,7 +8,6 @@ pushd "$BASEDIR"
 
 rm -rf build
 
-conan install libmysqlclient/8.0.31@#4e4b88008ebacf1867f24bcdd0c10e18 --generator markdown
 conan install . --output-folder=build --build=missing
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
