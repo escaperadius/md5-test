f2d1d7760db5250ad97c66716400d2ef25d6299f
escaperadius <chris@escaperadius.com>
2023-03-03 07:20:15 -0500

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -8,7 +8,7 @@ pushd "$BASEDIR"
 
 rm -rf build
 
-conan install . --output-folder=build --build=missing
+conan install . --output-folder=build --build=missing –require=poco/1.12.4
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
